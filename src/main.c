afdcbd65fd84a125a8fbc1153a64ba118d42b2e9
Sultan-Alshehry <192121920+Sultan-Alshehry@users.noreply.github.com>
2025-11-13 20:01:21 +0200

    update uart, not working yet

--- src/main.c
+++ src/main.c
@@ -8,96 +8,105 @@
 #include <queue.h>
 #include <task.h>
 
-#include "tkjhat/sdk.h"
 #include "buzzer.h"
+#include "tkjhat/sdk.h"
+#include "uart.h"
 
-#include "state.h"
-#include "interface.h"
 #include "buttons.h"
+#include "interface.h"
+#include "state.h"
 
-// Default stack size for the tasks. It can be reduced to 1024 if task is not using lot of memory.
-#define DEFAULT_STACK_SIZE 2048 
+// Default stack size for the tasks. It can be reduced to 1024 if task is not
+// using lot of memory.
+#define DEFAULT_STACK_SIZE 2048
 
-//Add here necessary states
-enum state { IDLE=1 };
+// Add here necessary states
+enum state { IDLE = 1 };
 enum state programState = IDLE;
 
-static void example_task(void *arg){
-    (void)arg;
+static void example_task(void *arg) {
+  (void)arg;
 
-    for(;;){
-        tight_loop_contents(); // Modify with application code here.
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
+  for (;;) {
+    tight_loop_contents(); // Modify with application code here.
+    vTaskDelay(pdMS_TO_TICKS(2000));
+  }
 }
 
 int main() {
-    stdio_init_all();
-    // Uncomment this lines if you want to wait till the serial monitor is connected
-    /*while (!stdio_usb_connected()){
-        sleep_ms(10);
-    }*/ 
-    init_hat_sdk();
+  stdio_init_all();
+  // Uncomment this lines if you want to wait till the serial monitor is
+  // connected
+  /*while (!stdio_usb_connected()){
+      sleep_ms(10);
+  }*/
+  init_hat_sdk();
 
-    state_init();
+  state_init();
 
-    sleep_ms(300); //Wait some time so initialization of USB and hat is done.
+  sleep_ms(300); // Wait some time so initialization of USB and hat is done.
 
+  TaskHandle_t myExampleTask, displayTask, buzzerTask, printTask, receiveTask;
+  // Create the tasks with xTaskCreate
+  BaseType_t result = xTaskCreate(
+      example_task,       // (en) Task function
+      "example",          // (en) Name of the task
+      DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words).
+                          // Generally 1024 or 2048
+      NULL,               // (en) Arguments of the task
+      2,                  // (en) Priority of this task
+      &myExampleTask);    // (en) A handle to control the execution of this task
 
-    TaskHandle_t myExampleTask, displayTask, buzzerTask;
-    // Create the tasks with xTaskCreate
-    BaseType_t result = xTaskCreate(example_task,       // (en) Task function
-                "example",              // (en) Name of the task 
-                DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,               // (en) Arguments of the task 
-                2,                  // (en) Priority of this task
-                &myExampleTask);    // (en) A handle to control the execution of this task
-
-    if(result != pdPASS) {
-        printf("Example Task creation failed\n");
-        return 0;
-    }
-
-    // Buzzer task
-    result = xTaskCreate(buzzer_task,   // (en) Task function
-            "buzzer",                   // (en) Name of the task 
-                1024,                   // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,                   // (en) Arguments of the task 
-                2,                      // (en) Priority of this task
-                &buzzerTask);
-  
-    // Display task
-    result = xTaskCreate(display_task,
-        "display",
-        DEFAULT_STACK_SIZE,
-        NULL,
-        2,
-        &displayTask
-    );
-
-    if (result != pdPASS) {
-        printf("Display Task creation failed \n");
-        return 0;
-    }
-
-    // Button task
-    result = xTaskCreate(button_task,
-        "button",
-        DEFAULT_STACK_SIZE,
-        NULL,
-        1,
-        &displayTask
-    );
-
-    if (result != pdPASS) {
-        printf("Button Task creation failed \n");
-        return 0;
-    }
-
-    // Start the scheduler (never returns)
-    vTaskStartScheduler();
-
-    // Never reach this line.
+  if (result != pdPASS) {
+    printf("Example Task creation failed\n");
     return 0;
-}
+  }
 
+  // Buzzer task
+  result = xTaskCreate(buzzer_task, // (en) Task function
+                       "buzzer",    // (en) Name of the task
+                       1024, // (en) Size of the stack for this task (in words).
+                             // Generally 1024 or 2048
+                       NULL, // (en) Arguments of the task
+                       2,    // (en) Priority of this task
+                       &buzzerTask);
+
+  // Display task
+  result = xTaskCreate(display_task, "display", DEFAULT_STACK_SIZE, NULL, 2,
+                       &displayTask);
+
+  if (result != pdPASS) {
+    printf("Display Task creation failed \n");
+    return 0;
+  }
+
+  result = xTaskCreate(print_task, "display", DEFAULT_STACK_SIZE, NULL, 2,
+                       &printTask);
+
+  if (result != pdPASS) {
+    printf("Display Task creation failed \n");
+    return 0;
+  }
+
+  result = xTaskCreate(receive_task, "display", DEFAULT_STACK_SIZE, NULL, 2,
+                       &receiveTask);
+
+  if (result != pdPASS) {
+    printf("Display Task creation failed \n");
+    return 0;
+  }
+  // Button task
+  result = xTaskCreate(button_task, "button", DEFAULT_STACK_SIZE, NULL, 1,
+                       &displayTask);
+
+  if (result != pdPASS) {
+    printf("Button Task creation failed \n");
+    return 0;
+  }
+
+  // Start the scheduler (never returns)
+  vTaskStartScheduler();
+
+  // Never reach this line.
+  return 0;
+}
